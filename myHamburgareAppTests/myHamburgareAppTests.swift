2789ef057dd65aafa631f7074093a6d8404ca64a
Alvar Arias <alvararias@gmail.com>
2026-04-24 16:19:37 +0200

    Add initial test structure for myHamburgareApp

---
+++ myHamburgareAppTests/myHamburgareAppTests.swift
@@ -0,0 +1,18 @@
+//
+//  myHamburgareAppTests.swift
+//  myHamburgareAppTests
+//
+//  Created by Alvar Arias on 2026-04-24.
+//
+
+import Testing
+
+struct myHamburgareAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
